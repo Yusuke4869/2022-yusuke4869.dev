0bb76ed7de11970523d5576aeeeb93f8cfdcebc9
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-01-10 13:37:27 +0900

    ✨ (Update): footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 import type { FC } from "react";
 
 const Footer: FC = () => (
-  <div>
-    <p>Footer</p>
-  </div>
+  <>
+    <hr className="opacity-75 my-2" />
+    <p className="flex justify-center text-sm py-4">&copy; 2022 Yusuke All Rights Reserved.</p>
+  </>
 );
 
 export default Footer;
