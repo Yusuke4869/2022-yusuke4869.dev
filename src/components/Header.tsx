0b5ffae70be3da262106e559512ae2b51b7ac675
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-01-10 13:18:32 +0900

    ✨ (Update): header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,29 @@
 import type { FC } from "react";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 const Header: FC = () => (
-  <div>
-    <p>Header</p>
+  <div className="sticky top-0 bg-zinc-800 opacity-75">
+    <div className="flex justify-evenly items-center py-2">
+      <div>
+        <Link href="/">
+          <a className="text-2xl p-2">Yusuke</a>
+        </Link>
+        <Link href="https://blog.growthers.dev/yusuke4869/">
+          <a className="text-xl ml-4 p-2" target="_blank" rel="noopener noreferrer">
+            blog
+          </a>
+        </Link>
+      </div>
+      <div>
+        <Link href="https://github.com/yusuke4869">
+          <a className="text-2xl" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+        </Link>
+      </div>
+    </div>
+    <hr className="opacity-75 mb-4" />
   </div>
 );
 
