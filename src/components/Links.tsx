8423a9f3649a26a4ab84dd49f4d3bacfd08e9fbe
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-06-14 06:24:10 +0900

    💬 (Add): keybase link

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,13 +1,23 @@
 import type { FC } from "react";
 import Link from "next/link";
 
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaKeybase } from "react-icons/fa";
 import { MdArticle } from "react-icons/md";
 
 const Links: FC = () => (
   <div>
     <p className="text-2xl underline underline-offset-2 decoration-orange-500">Links</p>
     <div className="flex pl-4 pt-2">
+      <Link href="https://keybase.io/yusuke4869">
+        <a
+          className="flex items-center text-lg p-2 hover:bg-gray-600 hover:rounded-2xl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaKeybase />
+          <p className="pl-1">Keybase</p>
+        </a>
+      </Link>
       <Link href="https://github.com/yusuke4869">
         <a
           className="flex items-center text-lg p-2 hover:bg-gray-600 hover:rounded-2xl"
